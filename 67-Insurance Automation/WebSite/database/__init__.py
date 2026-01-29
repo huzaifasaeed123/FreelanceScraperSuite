c7df4779daa76e2cab9d471608b32e690743e42e
Huzaifa Saeed <saeedhuzaifa678@gmail.com>
2026-01-30 03:59:36 +0500

    Fix Db Bug 2

--- 67-Insurance Automation/WebSite/database/__init__.py
+++ 67-Insurance Automation/WebSite/database/__init__.py
@@ -5,13 +5,11 @@ Database Package
 from .models import (
     init_database,
     get_connection,
-    DatabaseManager,
-    DATABASE_PATH
+    DatabaseManager
 )
 
 __all__ = [
     'init_database',
-    'get_connection', 
-    'DatabaseManager',
-    'DATABASE_PATH'
+    'get_connection',
+    'DatabaseManager'
 ]
